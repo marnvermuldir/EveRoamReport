90230350301ea5fc199a0bc75a21d0d68079543b
laserbeam3 <laserbeam3@users.noreply.github.com>
2017-04-25 17:37:45 +0200

    Initial working prototype.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Roam Report
+
+A tool for generating reports for a fleet roam.
+
+# Usage
+
+Input:
+ - The names of all pilots that flew in the roam
+ - Start time of the roam
+ - End time of the roam
+
+The tool then generates a list of kills that those pilots participated in. You can then manually deselect kills you're not interested in.
+
+Finally, generate an after action report template to be used in a phpBB forum.
+
+Designed to be used within EVE University forums. Very much WIP.
---
+++ roam.js
@@ -0,0 +1,218 @@
+// TODO:
+//  - Proper UI for list of kills.
+//  - Better date input.
+//      - Also UX for inputting hours (minutes are ignored by zkill API)
+//  - Defaults for date input (end = 4 hours form start).
+//  - Super fast select/deselect kill from final output:
+//      - Click and drag over kills?
+//      - Start/End separators?
+//  - Redo layout
+//      - Working concept: 3 resizable columns; A. Input names & start/end date; B. Filter kills; C. Output).
+//  - Correct list of ships (and updates when CCP releases new ones).
+//  - Input validation.
+//  - Progress/Spinner/Loading UI.
+//  - Abuse prevention and rate limiting (may require backend/caching).
+//  - Get system/region names.
+//  - Custom output formatting:
+//      - Different forum post templates.
+//      - Custom number formatting for isk values.
+//      - Allow killfeed.eveuni (not sure if worth, kill ids don't match between zkill and killfeed)
+//  - Discuss with Zarquu: Integration with the new SRP system.
+//  - Fleet chat log processing (to get fleet member names).
+//  - Other roam metrics>
+//      - Damage?
+//      - Member participation on kills?
+//      - Time?
+//      - Corps/Alliances involved?
+//      - Losses/Corp?
+//  - Ship icons?
+//  - Display additional data? (how many fleet members were part of each kill...?)
+//  - Detect missing fleet members?
+//  - Permalinks to roam reports?
+//  - Additional visualisations?
+//      - visual timeline?
+//      - parallel tracks for different systems?
+//  - Detect if browser is supported (obviously, Fetch API is not supported in IE).
+
+function numeric_sort(a, b) { return a-b; }
+
+function kill_sort(a, b)
+{
+    var d1 = new Date(a.killTime);
+    var d2 = new Date(b.killTime);
+    if (d1 > d2) return 1;
+    if (d1 == d2) return 0;
+    return -1;
+}
+
+function enter_names()
+{
+    var elem = document.getElementsByName("names")[0];
+    var nameList = elem.value.split("\n");
+    var finalNames = [];
+    window.killIDs = [];
+    window.unsortedKills = [];
+    window.friendlies = [];
+    for(var i = 0; i < nameList.length; ++i) {
+        var name = nameList[i].trim();
+        if (name) {
+            finalNames.push(escape(name));
+        }
+    }
+
+    var nameQuery = "https://api.eveonline.com/eve/CharacterID.xml.aspx?names=" + finalNames.join();
+    fetch(new Request(nameQuery, {method: 'GET'}))
+    .then(response => {
+        if (response.status != 200) throw new Error("API request failed to get list of character IDs");
+        return response.text();
+    })
+    .then(xmltext => {
+        // There really is no need to do xml processing...
+        var charIdRegex = /characterID="(\d+)"/gi;
+        var match;
+        while ((match = charIdRegex.exec(xmltext)) !== null) {
+            var id = parseInt(match[1]);
+            if (id != 0) window.friendlies.push(id);
+        }
+        window.friendlies = window.friendlies.sort(numeric_sort);
+        console.log("Got character IDs");
+        request_kill_batch(window.friendlies, 0);
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+}
+
+function request_kill_batch(characterIDs, start)
+{
+    console.log("Requesting kills for chars: " + start);
+    var group = characterIDs.slice(start, start+10);
+    var startdate = document.getElementsByName("startdate")[0].value.replace(/-/g, "");
+    var enddate = document.getElementsByName("enddate")[0].value.replace(/-/g, "");
+    var starttime = document.getElementsByName("starttime")[0].value;
+    var endtime = document.getElementsByName("endtime")[0].value;
+
+    if (starttime < 10) starttime = "0" + starttime.toString();
+    if (endtime < 10) endtime = "0" + endtime.toString();
+
+    var killQuery = "https://zkillboard.com/api/characterID/" + group.join() + "/startTime/"+startdate+starttime+"00/endTime/"+enddate+endtime+"00/no-items/";
+    fetch(new Request(killQuery, {method: 'GET', mode: 'cors'}))
+    .then(response => {
+        if (response.status != 200) throw new Error("API request failed to get list of character IDs");
+        return response.json();
+    })
+    .then(kills => {
+        var killAddCount = 0;
+        for (var i = 0; i < kills.length; ++i) {
+            var kill = kills[i];
+            if (window.killIDs.indexOf(kill.killID) >= 0) continue;
+            window.killIDs.push(kill.killID);
+            window.unsortedKills.push(kill);
+            killAddCount += 1;
+        }
+        console.log("Added " + killAddCount + " kills");
+        if (start+10 < characterIDs.length) {
+            request_kill_batch(characterIDs, start+10);
+        } else {
+            process_kills();
+        }
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+}
+
+
+function process_kills()
+{
+    console.log("Processing kills...");
+
+    window.wokringKills = window.unsortedKills.sort(kill_sort);
+    var table = document.getElementsByName("killdisplay")[0];
+    table.innerHTML = "<tr><th>Include?</th><th>Time</th><th>Kill/Loss</th><th>Ship</th><th>Victim</th><th>Final Blow</th><th>Location</th><th>ISK</th></tr>";
+    for (var i = 0; i < window.wokringKills.length; ++i) {
+        var kill = window.wokringKills[i];
+        var isFriendly = window.friendlies.indexOf(kill.victim.characterID) > -1;
+        kill.isFriendly = isFriendly;
+        var row = table.insertRow();
+        row.style.color = isFriendly ? "red" : "green";
+
+        var cell = row.insertCell();
+        cell.innerHTML = '<input type="checkbox" checked id="'+kill.killID+'" name="includecheck" value="'+kill.killID+'">';
+        // cell.children[0].checked = true;
+
+        cell = row.insertCell();
+        cell.innerHTML = "<a href=https://zkillboard.com/kill/"+kill.killID+"/>"+kill.killTime+"</a>";
+
+
+        cell = row.insertCell();
+        t = document.createTextNode(isFriendly ? "Loss" : "Kill");
+        cell.appendChild(t);
+
+        cell = row.insertCell();
+        var shipID = "" + kill.victim.shipTypeID;
+        var shipName = window.shipNames[shipID];
+        if (shipName === undefined) shipName = "Unknown";
+        t = document.createTextNode(shipName);
+        cell.appendChild(t);
+
+        cell = row.insertCell();
+        t = document.createTextNode(kill.victim.characterName);
+        cell.appendChild(t);
+
+        cell = row.insertCell();
+        t = document.createTextNode(kill.attackers.filter(x => x.finalBlow == 1)[0].characterName);
+        cell.appendChild(t);
+
+        cell = row.insertCell();
+        t = document.createTextNode(kill.solarSystemID);
+        cell.appendChild(t);
+
+        cell = row.insertCell();
+        t = document.createTextNode((Math.round(kill.zkb.totalValue/10000)/100) + "m");
+        cell.appendChild(t);
+    }
+}
+
+function get_forum_post()
+{
+    var lines = [];
+    var checkboxes = document.getElementsByName("includecheck");
+    var iskGain = 0;
+    var iskLoss = 0;
+    for (var i = 0; i < checkboxes.length; ++i) {
+        var box = checkboxes[i];
+        var id = box.value;
+        if (!box.checked) continue;
+
+        var kill = undefined
+        for (var i = 0; i < window.wokringKills.length; ++i) {
+            if (window.wokringKills[i].killID == id) {
+                kill = window.wokringKills[i];
+                break;
+            }
+        }
+
+        if (kill === undefined) continue;
+        var friendlyLine = kill.isFriendly ? "FF0000]-" : "00FF00]+";
+        lines.push("[url]https://zkillboard.com/kill/"+kill.killID+"/[/url] [color=#" + friendlyLine + (Math.round(kill.zkb.totalValue/10000)/100)+ "m[/color]");
+
+        if (!kill.isFriendly) {
+            iskGain += kill.zkb.totalValue;
+        } else {
+            iskLoss += kill.zkb.totalValue;
+        }
+    }
+
+    lines.push("\n");
+    var deltaColor = iskLoss < iskGain ? "[color=#00FF00]" : "[color=#FF0000]";
+
+    lines.push("ISK Destroyed: [color=#00FF00]" + iskGain.toLocaleString('en-US') +"[/color]");
+    lines.push("ISK Lost: [color=#FF0000]" + iskLoss.toLocaleString('en-US') +"[/color]");
+    lines.push("ISK Delta: " + deltaColor + (iskGain - iskLoss).toLocaleString('en-US') +"[/color]");
+    lines.push("Efficiency: " + deltaColor + ((iskGain*100) / (iskGain + iskLoss)).toLocaleString('en-US') +"%[/color]");
+
+    var elem = document.getElementsByName("output")[0];
+    elem.value = lines.join("\n");
+}
+
